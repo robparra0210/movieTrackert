9b392141039934858375edee7fe93c81c1382895
rob0210 <robertoparra0210@gmail.com>
2020-06-21 18:46:09 -0400

    🚩ADD: Validation Strings, Titles, and director
    Se coloco una funcion para validar si se ingrea una cadena de texto o
    no, tambien se coloco una funcion donde se valide que el titulo de la
    pelicula no contenga mas de 100 caracteres y el nombre del director
    no mas de 50, en caso de superar esas cantidades se truncara al final
    con tres puntos supensivos '...'

--- pelicula.js
+++ pelicula.js
@@ -14,21 +14,46 @@ class Pelicula {
 		this.calificacion =calificacion;
 		
 		this.validarId(id)
+		this.validarTituloYDirector(titulo,director)
 		}
 
+
+	validarCadenaTexto(texto){
+		if(typeof(texto)=='string'){
+			return
+		}else{
+			return console.error(`${texto} no corresponde a una cadena de texto por favor verifique el campo`)
+		}
+
+	}
+
 	validarId(id){
+		this.validarCadenaTexto(id)
 		if(!(/^([a-z]){2}([0-9]){7}$/.test(id))){  /*no le coloco un else porque en la validacion interesa mas el caso incorrecto para
 													devolver a pantalla */
 				return console.error(`IMDB ID ${id} no es valido debe de contener 9 caracteres donde los dos primeros sean letras y
 				otros 7 numeros`)
 				}
 		}
+	
+	validarTituloYDirector(titulo,director){
+		this.validarCadenaTexto(titulo)
+		this.validarCadenaTexto(director)
+		titulo.length>=100?titulo=titulo.slice(0,100)+'...':titulo =titulo
+		director.length>=50?director=director.slice(0,50)+'...':director =director
+		this.titulo = titulo
+		this.director =director
+		return 
+		}
+
+
 	}
 
+
 var interstellar = new Pelicula(
-	{	id: 'tt0816692',		//se deben pasar objetos para qie la destructurasion funciones 
+	{	id: 'tt0841669',		//se deben pasar objetos para qie la destructurasion funciones 
 		titulo: 'interstellar',
-		director: 'Christopher Nolan',
+		director: 'Christopher Nolan ',
 		estreno: 2014,
 		pais:['EE.UU','U.K','canada'],
 		generos:['adventure', 'Drama', 'Sci-Fi'],
